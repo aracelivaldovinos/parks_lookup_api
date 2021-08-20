cb85f4317e126994418420da403574dea1e85916
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-08-20 12:39:46 -0500

    seeded table

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,12 +1,36 @@
 class ParksController < ApplicationController
 
   def index
-    @parks = {"Texas": "Big Bend National Park"}
+    @parks = Park.all
     json_response(@parks)
+  end
+
+  def show
+    @park = Park.find(params[:id])
+    json_response(@park)
+  end
+
+  def create
+    @park = Park.create(park_params)
+    json_response(@park)
+  end
+
+  def update
+    @park = Park.find(params[:id])
+    @park.update(park_params)
+  end
+
+  def destroy
+    @park = Park.find(params[:id])
+    @park.destroy
   end
 
   private
   def json_response(object, status = :ok)
     render json: object, status: status
   end
-end
+
+  def park_params
+    params.permit(:area, :description, :state, :name)
+  end
+end 
