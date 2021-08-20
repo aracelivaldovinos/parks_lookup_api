074691bf38725544b4509bed18e2244e087ad9e7
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-08-20 17:02:27 -0500

    passing integration for get route

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,4 +5,10 @@ FactoryBot.define do
     name {Faker::Mountain.name}
     state {Faker::Address.state}
   end
+  factory(:bird) do
+    family_name {Faker::Creature::Bird.common_family_name}
+    common_name {Faker::Creature::Bird.common_name}
+    color {Faker::Creature::Bird.color} 
+    geography {Faker::Creature::Bird.geo}
+  end
 end
---
+++ spec/requests/get_birds_spec copy.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe "get all birds route", :type => :request do
+  let!(:birds) { FactoryBot.create_list(:bird, 3)}
+
+  before { get '/birds'}
+
+  it 'returns all birds' do
+    expect(JSON.parse(response.body).size).to eq(3)
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
+
+# describe "returns first 10 birds", :type => :request do
+#   let!(:birds) { FactoryBot.create_list(:bird, 20)}
+
+#   before { get '/birds?page=1'}
+
+#   it 'returns 10 birds' do
+#     expect(JSON.parse(response.body).size).to eq(10)
+#   end
+
+#   it 'returns status code 200' do
+#     expect(response).to have_http_status(:success)
+#   end
+end
